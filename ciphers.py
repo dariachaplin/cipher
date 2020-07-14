7de85136860331066815122579fa95ef3859e0c2
Daria Chaplin <dxc4643@g.rit.edu>
2020-07-13 22:20:15 -0400

    Initial set-up commit, added .gitignore to avoid PyCharm issues

---
+++ ciphers.py
@@ -0,0 +1,9 @@
+# File: ciphers.py
+# Author: Daria Chaplin
+# Info: Contains functions for various methods of encryption/decryption
+
+# probably start with caesar! can move on to substitution, affine, enigma?
+
+def test():
+    print("ciphers test")
+    # placeholder for now
---
+++ driver.py
@@ -0,0 +1,11 @@
+# File: driver.py
+# Author: Daria Chaplin
+# Info: Passes given parameters off to appropriate functions in ciphers.py
+
+import ciphers
+
+def main():
+    ciphers.test()
+
+if __name__=="__main__":
+    main()
